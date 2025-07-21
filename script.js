2fc182befd8017113119786aeb4ad6ab93949666
wrth1337 <mail@hellowirth.de>
2025-07-22 00:15:31 +0200

    Adjust animation.

--- script.js
+++ script.js
@@ -2,26 +2,30 @@ const element = document.getElementById("typewriter-text");
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
+const getRandomNumber = (min, max) => {
+  return Math.random() * (max - min) + min
+}
+
 async function typeWriterEffect() {
   const fullText = "Hello World!";
-  const replacement = "Wirth!";
-  const base = "Hello ";
+  const replacement = "irth!";
+  const base = "Hello W";
 
   for (let i = 0; i <= fullText.length; i++) {
     element.textContent = fullText.substring(0, i);
-    await delay(100);
+    await delay(getRandomNumber(75,105));
   }
 
-  await delay(800);
+  await delay(450);
 
   for (let i = fullText.length; i >= base.length; i--) {
     element.textContent = fullText.substring(0, i);
-    await delay(80);
+    await delay(getRandomNumber(65,100));
   }
 
   for (let i = 0; i <= replacement.length; i++) {
     element.textContent = base + replacement.substring(0, i);
-    await delay(100);
+    await delay(getRandomNumber(100,140));
   }
 }
 
